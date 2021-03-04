3e7248050f4c44b0b5a2c3c88f78d3f314db1f87
Logan <58321687+Smyli3@users.noreply.github.com>
2021-03-04 10:50:26 -0800

    Commented on code

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+//Using basic three in addition to UnityEngine.UI which allows for user input
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
